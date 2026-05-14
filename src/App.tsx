8a921845ecdbd4432457a06e352f39cc809c71ae
vhendala <vhendala@gmail.com>
2026-05-13 20:14:44 -0400

    Refactor landing page and clean up project

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import VantsLandingPage from './Component';
-import { LoaderDemo } from './components/LoaderDemo';
-
-export default function App() {
-  // Simple route handling - check if URL contains ?demo=loader
-  const isLoaderDemo = window.location.search.includes('demo=loader');
-
-  return isLoaderDemo ? <LoaderDemo /> : <VantsLandingPage />;
-}
